dba473e5d615b6d105333146b7057dd32ed7bfbd
Megharth <m.c.lakhataria@gmail.com>
2021-07-22 13:23:26 +0530

    make mastermind

--- Game.rb
+++ Game.rb
@@ -1,153 +1,106 @@
-# To register the player
 class Player
-    attr_reader :name
-    attr_accessor :mark
-    def initialize(name)
+    attr_reader :name, :role
+    def initialize(name, role, type)
         @name = name
-        @mark = ''
-    end
-end
-
-# Represents the Tic-Tac-Toe game
-class Game
-    attr_reader :turn
-
-    # initialize the game with two players
-    def initialize(p1, p2)
-        @p1 = p1
-        @p2 = p2
-        @turn = p1
-        @boardRef = [['1, 1', '1, 2', '1, 3'], ['2, 1', '2, 2', '2, 3'], ['3,1 ', '3, 2', '3, 3']]
-        @board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
-        p1.mark = 'X'
-        p2.mark = 'O'
+        @role = role
+        @type = type
+        @colors = [:red, :blue, :green, :white, :black, :yellow, :orange, :magenta, :cyan]
     end
 
-    def changeTurn 
-        @turn = @turn == @p1 ? @p2 : @p1
+    def setCode
+        if(@type == :computer)
+            @code = [@colors.sample, @colors.sample, @colors.sample, @colors.sample]
+        end
     end
 
-    # Mark the box and change the turn
-    def markBox ip
-        block = ip.split(' ')
-        box = @board[block[0].to_i - 1][block[1].to_i - 1]
-        if box.include? 'X' or box.include? 'O'
-            puts 'Invalid move'
+    def guessCode
+        begin
+            puts "Guess the code"
+            code = gets.chomp
+            code = code.split(' ')
+            code = code.map do |color| 
+                color.downcase.to_sym
+            end
+            return code
+        rescue Interrupt
+            puts "\nGame ended :("
+            return nil
+        end
+    end
+
+    def verifyCode code
+        if code == @code
+            return [:r, :r, :r, :r]
         else
-            @board[block[0].to_i - 1][block[1].to_i - 1] = @turn.mark
-            changeTurn()
-        end
-    end
-
-    # Print a reference tic-tac-toe board
-    def printReference
-        @boardRef.each_with_index do |row, i|
-            puts " #{row[0]} | #{row[1]} | #{row[2]} "
-            if i != 2
-                puts "-------------------"
-            end
-        end
-    end
-
-    # Print the actual state of the board
-    def printBoard
-        @board.each_with_index do |row, i|
-            puts " #{row[0]} | #{row[1]} | #{row[2]} "
-            if i != 2
-                puts "------------"
-            end
-        end
-    end
-
-    # Check if all the boxes in the board are filled
-    def boardEmpty
-        @board.each do |row|
-            row.each do |col|
-                if col.strip == ''
-                    return true
+            result = Array.new
+            code.each_with_index do |color, ii|
+                if @code.include? color
+                    if @code[ii] == color
+                        result.push :r
+                    else
+                        result.push :w
+                    end
+                else
+                    result.push('-')
                 end
             end
+            return result 
         end
-        return false
-    end
-    
-    # Check if the game is over
-    # cond1: Its a truce
-    # cond2: One of the players won
-    def gameOver
-        conditions = [
-            # diagonals
-            (@board[0][0] == @board[1][1] and @board[1][1] == @board[2][2] and 
-                @board[0][0].strip != '' and @board[1][1].strip != '' and @board[2][2].strip != ''),
-            (@board[0][2] == @board[1][1] and @board[1][1] == @board[2][0] and
-                @board[0][2].strip != '' and @board[1][1].strip != '' and @board[2][0].strip != ''),
-            
-            # rows
-            (@board[0][0] == @board[0][1] and @board[0][1] == @board[0][2] and 
-                @board[0][0].strip != '' and @board[0][1].strip != '' and @board[0][2].strip != ''),
-            (@board[1][0] == @board[1][1] and @board[1][1] == @board[1][2] and 
-                @board[1][0].strip != '' and @board[1][1].strip != '' and @board[1][2].strip != ''),
-            (@board[2][0] == @board[2][1] and @board[2][1] == @board[2][2] and 
-                @board[2][0].strip != '' and @board[2][1].strip != '' and @board[2][2].strip != ''),
-            
-            # cols
-            (@board[0][0] == @board[1][0] and @board[1][0] == @board[2][0] and
-                @board[0][0].strip != '' and @board[1][0].strip != '' and @board[2][0].strip != ''),
-            (@board[0][1] == @board[1][1] and @board[1][1] == @board[2][1] and 
-                @board[0][1].strip != '' and @board[1][1].strip != '' and @board[2][1].strip != ''),
-            (@board[0][2] == @board[1][2] and @board[1][2] == @board[2][2] and 
-                @board[0][2].strip != '' and @board[1][2].strip != '' and @board[2][2].strip != ''),
-        ] 
-
-
-        conditions.each do |cond|
-            if cond
-                return true
-            end
-        end
-
-        if !boardEmpty()
-            @truce = true
-            return true
-        end
-
-        return false
-    end
-
-    # starts the game
-    def start 
-        while !gameOver() do
-            printReference()
-            puts
-            printBoard()
-            puts "#{@turn.name}, please provide input"
-            begin
-                ip = gets.chomp
-            rescue Interrupt => e
-                @gameInterrupt = true
-                break
-            else
-                markBox(ip)
-                puts `clear`
-            end
-        end
-
-        if @truce 
-            printBoard()
-            puts "Whoops! It's a truce"
-        elsif @gameInterrupt
-            puts "Game ended :("
-        else 
-            printBoard()
-            changeTurn()
-            puts "Congratulations #{@turn.name}, you won!"
-        end
-
     end
 end
 
-p1 = Player.new "Megharth"
-p2 = Player.new "Bhoomi"
+class Game
+    def initialize(name)
+        @codeBreaker = Player.new(name, :codeBreaker, :human)
+        @codeMaker = Player.new("Computer", :codeMaker, :computer)
+        @trials = 12
+        @history = Array.new
+    end
 
-game = Game.new p1, p2
-game.start
+    def printBoard
+        puts `clear`
+        @history.each do |code|
+            puts '+-------------------------------------------+' 
+            puts "| #{code[0]} | #{code[1]} | #{code[2]} | #{code[3]} | #{code[4]} |"
+            puts '+-------------------------------------------+' 
+        end
+    end
+
+    def printScore
+        puts '+----------------------------------+'
+        puts "| #{@codeMaker.name}      |      #{@history.length - 1}      |"
+        puts '+----------------------------------+'
+        puts "| #{@codeBreaker.name}      |      #{@trials - @history.length + 1}      |"
+        puts '+----------------------------------+'
+
+    end
+
+    def start
+        @codeMaker.setCode()
+        @trials.times do 
+            code = @codeBreaker.guessCode()
+            if code != nil
+                results = @codeMaker.verifyCode(code)
+                code.push(results.join(' '))
+                @history.push(code)
+                printBoard()
+
+                if results == [:r, :r, :r, :r]
+                    puts "Congratulations #{@codeBreaker.name}, you managed to break the code!"
+                    break
+                end
+            else
+                break
+            end
+        end
+
+        if @history.length == 12 
+            puts "Whoops #{@codeBreaker.name}, you couldn't break the code!"
+        end
+
+        printScore()
+    end
+end
+
+game = Game.new("Megharth")
+game.start()
